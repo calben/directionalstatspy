3ece7a8ddf9c282abe0da5a39c12d89172f7adff
Calem Bendell <calem.j.bendell@gmail.com>
2014-09-14 22:40:54 -0400

    test with sample files and move samples

--- directional/angular.py
+++ directional/angular.py
@@ -14,8 +14,8 @@ def col_names_from_sin_cos_matrix(df):
 
 def sin_cos_matrix_to_radian_matrix(sin_cos_matrix):
   radian_matrix = pd.DataFrame(index = sin_cos_matrix.index)
-  for name in [col[:-5] for col in sin_cos_matrix.columns]:
-    radian_matrix[name] = np.arctan2(sin_cos_matrix[name + "--SIN"].values, sin_cos_matrix[name + "--COS"].values)
+  for col in col_names_from_sin_cos_matrix(sin_cos_matrix):
+    radian_matrix[col] = np.arctan2(sin_cos_matrix[col + "--SIN"].values, sin_cos_matrix[col + "--COS"].values)
   return radian_matrix
 
 
@@ -31,14 +31,14 @@ def sin_cos_matrix_to_degrees_matrix(sin_cos_matrix):
   return sin_cos_matrix_to_radian_matrix(sin_cos_matrix).applymap(np.degrees)
 
 
-def sin_cos_pair_std(sin_cos_matrix):
+def sin_cos_matrix_std(sin_cos_matrix):
   mean = sin_cos_pair_mean(sin_cos_matrix)
   result = pd.Series(index = sin_cos_matrix.index)
   for name in col_names_from_sin_cos_matrix(sin_cos_matrix):
     pass
 
 
-def sin_cos_pair_mean(sin_cos_matrix):
+def sin_cos_matrix_mean(sin_cos_matrix):
   return sin_cos_matrix.mean()
 
 
--- tests/test_angular.py
+++ tests/test_angular.py
@@ -2,9 +2,14 @@ from directional import *
 import pandas as pd
 import numpy as np
 
+demo_sin_cos_matrix = pd.read_csv("sample_data/sin-cos.csv")
+demo_angle_matrix = pd.read_csv("sample_data/degrees.csv")
+
 def test_col_names_from_sin_cos_matrix():
 	df = pd.DataFrame(columns = ["N-CA-CB-CG--SIN", "CA-CB-CG-OD1--SIN", "CA-CB-CG-OD2--SIN", "N-CA-CB-CG--COS", "CA-CB-CG-OD1--COS", "CA-CB-CG-OD2--COS"])
 	cols = col_names_from_sin_cos_matrix(df)
 	assert cols == ["N-CA-CB-CG", "CA-CB-CG-OD1", "CA-CB-CG-OD2"]
 
 
+def test_sin_cos_matrix_to_degrees_matrix():
+	assert demo_angle_matrix.values == sin_cos_matrix_to_degrees_matrix(demo_sin_cos_matrix).values
