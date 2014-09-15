fe0e8e9ed7bdf73bb1f2ff545fa312e4a0b27f14
Calem Bendell <calem.j.bendell@gmail.com>
2014-09-14 23:01:09 -0400

    coverage for sin_cos to radian

--- tests/test_angular.py
+++ tests/test_angular.py
@@ -4,6 +4,7 @@ import numpy as np
 
 demo_sin_cos_matrix = pd.read_csv("sample_data/sin-cos.csv")
 demo_angle_matrix = pd.read_csv("sample_data/degrees.csv")
+demo_radian_matrix = pd.read_csv("sample_data/radians.csv")
 
 def test_col_names_from_sin_cos_matrix():
 	df = pd.DataFrame(columns = ["N-CA-CB-CG--SIN", "CA-CB-CG-OD1--SIN", "CA-CB-CG-OD2--SIN", "N-CA-CB-CG--COS", "CA-CB-CG-OD1--COS", "CA-CB-CG-OD2--COS"])
@@ -13,6 +14,15 @@ def test_col_names_from_sin_cos_matrix():
 
 def test_sin_cos_matrix_to_degrees_matrix():
 	result = sin_cos_matrix_to_degrees_matrix(demo_sin_cos_matrix).values
-	result = np.around(result, decimals = 1)
-	comparison = np.around(demo_angle_matrix.values, decimals = 1)
+	result = np.around(result, decimals = 2)
+	comparison = np.around(demo_angle_matrix.values, decimals = 2)
 	assert np.array_equal(comparison, result) 
+
+
+def test_sin_cos_matrix_to_radian_matrix():
+	result = sin_cos_matrix_to_radian_matrix(demo_sin_cos_matrix.applymap(np.radians)).values
+	result = np.around(result, decimals = 2)
+	comparison = np.around(demo_radian_matrix.values, decimals = 2)
+	assert np.array_equal(comparison, result) 
+
+
